fc677e76b53dab7219e2428b2372499d52a8d60f
shirishshrestha <shirish.shrestha07@gmail.com>
2025-11-17 15:30:26 +0545

    change: index.js layout

--- features/panel/admin/journal/hooks/index.js
+++ features/panel/admin/journal/hooks/index.js
@@ -3,12 +3,30 @@ export { useGetJournalById } from "./query/useGetJournalById";
 export { useGetTaxonomyTree } from "./query/useGetTaxonomyTree";
 export { useGetJournalStaff } from "./query/useGetJournalStaff";
 export { useGetUsers } from "./query/useGetUsers";
-
-export { useGetTaxonomyTree } from "./query/useGetTaxonomyTree";
 export { useCreateJournal } from "./mutation/useCreateJournal";
 export { useUpdateJournal } from "./mutation/useUpdateJournal";
-export { useCreateSection, useUpdateSection, useDeleteSection } from "./mutation/useSectionMutations";
-export { useCreateCategory, useUpdateCategory, useDeleteCategory } from "./mutation/useCategoryMutations";
-export { useCreateResearchType, useUpdateResearchType, useDeleteResearchType } from "./mutation/useResearchTypeMutations";
-export { useCreateArea, useUpdateArea, useDeleteArea } from "./mutation/useAreaMutations";
-export { useAddJournalStaff, useUpdateJournalStaff, useRemoveJournalStaff } from "./mutation/useStaffMutations";
+export {
+  useCreateSection,
+  useUpdateSection,
+  useDeleteSection,
+} from "./mutation/useSectionMutations";
+export {
+  useCreateCategory,
+  useUpdateCategory,
+  useDeleteCategory,
+} from "./mutation/useCategoryMutations";
+export {
+  useCreateResearchType,
+  useUpdateResearchType,
+  useDeleteResearchType,
+} from "./mutation/useResearchTypeMutations";
+export {
+  useCreateArea,
+  useUpdateArea,
+  useDeleteArea,
+} from "./mutation/useAreaMutations";
+export {
+  useAddJournalStaff,
+  useUpdateJournalStaff,
+  useRemoveJournalStaff,
+} from "./mutation/useStaffMutations";
